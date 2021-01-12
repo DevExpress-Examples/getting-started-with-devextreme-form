92692afc294127d50611b2e9f0fa18f2a30c0233
Alexander Yakovlev <alexander.yakovlev@devexpress.com>
2021-01-12 12:39:25 +0300

    Add Vue code

---
+++ vue/src/App.vue
@@ -0,0 +1,150 @@
+<template>
+  <div id="app-container">
+    <form action="/employee-page" @submit="handleSubmit">
+      <DxForm 
+        id="form"
+        :form-data="employee"
+        :read-only="isFormReadOnly"
+        label-location="top"
+        :show-colon-after-label="false">
+        <DxGroupItem
+            caption="Personal Information"
+            :col-count="2">
+            <DxGroupItem caption="employee">
+              <DxSimpleItem data-field="name" :is-required="true"/>
+              <DxSimpleItem data-field="position"/>
+              <DxSimpleItem data-field="hireDate"/>
+              <DxSimpleItem data-field="officeNumber">
+                  <DxNumericRule
+                      message="This field should contain a number"
+                  />
+              </DxSimpleItem>
+            </DxGroupItem>
+            <DxGroupItem caption="Personal Information">
+              <DxTabbedItem>
+                <DxTabPanelOptions
+                  :height="320" :element-attr="{ id: 'tabbedItem' }"
+                />
+                <DxTab title="Contacts">
+                  <DxSimpleItem data-field="phone"/>
+                  <DxSimpleItem data-field="skype"/>
+                  <DxSimpleItem data-field="email">
+                      <DxEmailRule
+                          message="This is not a valid Email"
+                      />
+                  </DxSimpleItem>
+                </DxTab>
+                <DxTab title="Note">
+                  <DxSimpleItem 
+                    data-field="notes"
+                    editor-type="dxTextArea"
+                  />
+                </DxTab>
+              </DxTabbedItem>
+            </DxGroupItem>
+        </DxGroupItem>
+        <DxButtonItem 
+          :button-options="submitButtonOptions"
+          horizontal-alignment="center"
+        />
+      </DxForm>
+    </form>
+
+    <DxCheckBox
+      id="check-box"
+      text="Enable read-only mode"
+      v-model="isFormReadOnly"
+    />
+  </div>
+</template>
+
+<script>
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+
+import { 
+    DxForm, 
+    DxSimpleItem, 
+    DxGroupItem,
+    DxButtonItem,
+    DxTabbedItem,
+    DxTab,
+    DxTabPanelOptions,
+    DxNumericRule, 
+    DxEmailRule
+} from 'devextreme-vue/form';
+
+import { DxCheckBox } from 'devextreme-vue/check-box';
+
+const employee = {
+    name: 'John Heart',
+    position: 'CEO',
+    hireDate: new Date(2012, 4, 13),
+    officeNumber: 901,
+    phone: '+1(213) 555-9392',
+    skype: 'jheart_DX_skype',
+    email: 'jheart@dx-email.com',
+    notes: 'John has been in the Audio/Video industry since 1990.'
+};
+
+const submitButtonOptions = {
+    text: "Submit the Form",
+    useSubmitBehavior: true
+};
+
+let isFormReadOnly = false;
+
+export default {
+  name: "app",
+    components: {
+        DxForm,
+        DxSimpleItem,
+        DxGroupItem,
+        DxButtonItem,
+        DxTabbedItem,
+        DxTab,
+        DxTabPanelOptions,
+        DxNumericRule, 
+        DxEmailRule,
+        DxCheckBox
+    },
+    data() {
+        return {
+          employee,
+          submitButtonOptions,
+          isFormReadOnly
+        }
+    },
+    methods: {
+        handleSubmit(e) {
+            setTimeout(() => { 
+                alert("Submitted");          
+            }, 1000);
+            
+            e.preventDefault();
+        }
+    }
+}
+</script>
+
+<style scoped>
+#form {
+  height: 38em;
+  margin: 20px 0 0 20px;
+  padding: 10px;
+  border: 1px solid;
+}
+
+#check-box {
+  margin: 20px;
+}
+
+#app-container {
+  width: 900px;
+  position: relative;
+}
+
+#tabbedItem {
+  margin-top: 18px;
+}
+</style>
