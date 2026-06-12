5d0a415eacee0579baaf9e112d1ed341d71c910a
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 14:20:37 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 import { Employee, SubmitButtonOptions } from './app.types';
 
@@ -9,6 +9,7 @@ import { DxCheckBoxModule } from 'devextreme-angular/ui/check-box';
     selector: 'app-root',
     imports: [DxFormModule, DxCheckBoxModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
