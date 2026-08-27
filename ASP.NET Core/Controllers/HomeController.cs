5b4290389beeaa5ec2ca55bbbee21054d7fd4440
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2026-08-27 07:18:56 +0400

    Add ASP.NET Core Example

---
+++ ASP.NET Core/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP_NET_Core.Models;
+
+namespace ASP_NET_Core.Controllers;
+public class HomeController: Controller {
+    public IActionResult Index() {
+        return View(FormData.FormDataSource);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+        return View();
+    }
+}
